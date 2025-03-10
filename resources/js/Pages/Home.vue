8587fb8eda041b900ad04afcdcb1c78b0a155f6d
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2025-03-10 00:36:02 -0600

    Configuracion🚀

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Bienvenido a Laravel con Inertia y Vue</h1>
+    </div>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css';
+import { createVuetify } from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+const vuetify = createVuetify();
+
+createInertiaApp({
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .mount(el);
+    },
+});
