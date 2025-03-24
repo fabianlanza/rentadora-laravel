792354c7f0cb7967c3d4f7341250f7f87c35a194
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2025-03-24 01:16:09 -0600

    Fix Nuevo metodo de importar Vuetify

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,51 @@
 <template>
-    <div>
-        <h1>Bienvenido a Laravel con Inertia y Vue</h1>
-    </div>
+    <v-card>
+        <v-layout>
+            <v-navigation-drawer
+                expand-on-hover
+                rail
+            >
+                <v-list>
+                    <v-list-item
+                        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                        subtitle="fabianlanza@gmail.com"
+                        title="Sandra Adams"
+                    ></v-list-item>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                <v-list density="compact" nav>
+                    <v-list-item prepend-icon="mdi-hospital" title="Menu" value="myfiles"></v-list-item>
+                    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+                    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+
+            <v-app>
+                <h1>Bienvenido</h1>
+            </v-app>
+        </v-layout>
+    </v-card>
 </template>
+
+
+
+<script>
+export default {
+    name: "Home",
+    props: {
+        registros: {
+            type: Array,
+            default: () => []
+        },
+    },
+    data(){
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,24 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import '../css/app.css';
-import { createVuetify } from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
 
-const vuetify = createVuetify();
+// Nueva forma de importar Vuetify 3, metodo del ING no funcionaba
+import 'vuetify/styles'; // Importar estilos de Vuetify
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(vuetify)
+            .use(vuetify) // Agregar Vuetify
             .mount(el);
     },
 });
