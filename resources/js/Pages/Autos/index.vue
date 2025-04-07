1334fabe8c24da958a903e1e19288559237ad099
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2025-04-07 01:49:23 -0600

    Implementacion MainLayout en Autos

--- resources/js/Pages/Autos/index.vue
+++ resources/js/Pages/Autos/index.vue
@@ -1,26 +1,6 @@
 <template>
-    <v-app>
-        <!-- Nav Bar-->
-        <v-app-bar app color="#eeeeee" elevation="0">
-            <v-container class="d-flex justify-space-between align-center">
-                <v-img src="/images/logo.png" max-height="40" max-width="120" contain></v-img>
-                <v-spacer></v-spacer>
+    <MainLayout :TabSeleccionada="1">
 
-                <v-tabs v-model="TabSeleccionada" centered>
-                    <v-tab tag="div" @click="$inertia.get('/')">Inicio</v-tab>
-                    <v-tab tag="div" @click="$inertia.get('/autos')">Autos</v-tab>
-                    <v-tab tag="div" @click="$inertia.get('/contacto')">Contacto</v-tab>
-                </v-tabs>
-                <v-spacer></v-spacer>
-
-                <div class="d-flex gap-2">
-                    <v-btn color="#00a9d4" variant="elevated" @click="$inertia.get('/login')">Login</v-btn>
-                    <v-btn color="#00a9d4" variant="outlined" @click="$inertia.get('/register')" class="ml-2">Register</v-btn>
-                </div>
-            </v-container>
-        </v-app-bar>
-
-        <v-main>
             <v-container style="max-width: 75vw; margin: 0 auto;">
                 <h1 class="text-center my-6">Autos Disponibles</h1>
 
@@ -34,11 +14,11 @@
                     </template>
                 </v-row>
             </v-container>
-        </v-main>
-    </v-app>
+        </MainLayout>
 </template>
 
 <script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
 import { defineProps, ref } from 'vue';
 import CarCard from '@/components/CarCard.vue';
 
@@ -47,8 +27,9 @@ const props = defineProps({
     autos: Array, // Asegúrate de que sea un array de autos
 });
 
+
 // Constante para decirle a las tab que Autos esta seleccionado
-const TabSeleccionada = ref(1);
+// const TabSeleccionada = ref(1);
 </script>
 
 <style scoped>
