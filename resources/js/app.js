6fba29a8319ec4d6b375a43e66ed66dc52500874
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2025-04-09 11:57:00 -0600

    fix typo

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import vuetify from './Plugins/vuetify.js'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'ViaCar';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
